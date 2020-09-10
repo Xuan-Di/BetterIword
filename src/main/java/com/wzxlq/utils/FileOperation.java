459086c4a9f346faac69521195067a31118755c1
wzx <1099393970@qq.com>
2020-09-10 10:23:20 +0800

    背单词重构完整

--- src/main/java/com/wzxlq/utils/FileOperation.java
+++ src/main/java/com/wzxlq/utils/FileOperation.java
@@ -8,13 +8,20 @@ import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
-// 文件相关操作
 public class FileOperation {
 
-    // 读取文件名称为filename中的内容，并将其中包含的所有词语放进words中
-    public static boolean readFile(String filename, ArrayList<String> words){
+    /**
+     * 功能描述
+     * 读取文件名称为filename中的内容，并将其中包含的所有词语放进words中
+     *
+     * @param filename
+     * @param words
+     * @return boolean
+     */
 
-        if (filename == null || words == null){
+    public static boolean readFile(String filename, ArrayList<String> words) {
+
+        if (filename == null || words == null) {
             System.out.println("filename is null or words is null");
             return false;
         }
@@ -24,26 +31,21 @@ public class FileOperation {
 
         try {
             File file = new File(filename);
-            if(file.exists()){
+            if (file.exists()) {
                 FileInputStream fis = new FileInputStream(file);
                 scanner = new Scanner(new BufferedInputStream(fis), "UTF-8");
                 scanner.useLocale(Locale.ENGLISH);
-            }
-            else
+            } else {
                 return false;
-        }
-        catch(IOException ioe){
+            }
+        } catch (IOException ioe) {
             System.out.println("Cannot open " + filename);
             return false;
         }
 
         // 简单分词
-        // 这个分词方式相对简陋, 没有考虑很多文本处理中的特殊问题
-        // 在这里只做demo展示用
         if (scanner.hasNextLine()) {
-
             String contents = scanner.useDelimiter("\\A").next();
-
             int start = firstCharacterIndex(contents, 0);
             for (int i = start + 1; i <= contents.length(); )
                 if (i == contents.length() || !Character.isLetter(contents.charAt(i))) {
@@ -58,11 +60,19 @@ public class FileOperation {
         return true;
     }
 
-    // 寻找字符串s中，从start的位置开始的第一个字母字符的位置
-    private static int firstCharacterIndex(String s, int start){
+    /**
+     * 功能描述
+     * 寻找字符串s中，从start的位置开始的第一个字母字符的位置
+     *
+     * @param s
+     * @param start
+     * @return int
+     */
 
-        for( int i = start ; i < s.length() ; i ++ )
-            if( Character.isLetter(s.charAt(i)) )
+    private static int firstCharacterIndex(String s, int start) {
+
+        for (int i = start; i < s.length(); i++)
+            if (Character.isLetter(s.charAt(i)))
                 return i;
         return s.length();
     }
